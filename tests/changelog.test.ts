51d711335ffb03106ea7c4fa57fe8ba6d59fbd91
Jeff Repanich <jrepanich@gmail.com>
2026-08-03 14:07:11 -0400

    test: replace verification scripts with vitest

---
+++ tests/changelog.test.ts
@@ -0,0 +1,15 @@
+import { readFile } from "node:fs/promises";
+import { expect, test } from "vitest";
+
+test("changelog has unreleased and current-version sections", async () => {
+  const [manifestSource, changelog] = await Promise.all([
+    readFile(new URL("../package.json", import.meta.url), "utf8"),
+    readFile(new URL("../CHANGELOG.md", import.meta.url), "utf8"),
+  ]);
+  const { version } = JSON.parse(manifestSource) as { version: string };
+
+  expect(changelog).toMatch(/^## \[Unreleased\]$/m);
+  expect(changelog).toMatch(
+    new RegExp(`^## \\[${version.replaceAll(".", "\\.")}\\] - \\d{4}-\\d{2}-\\d{2}$`, "m"),
+  );
+});
